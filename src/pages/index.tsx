3d609aa721bdf66342762c6137d9b9907da5d31b
David Barros <dwbarros@gmail.com>
2021-12-28 11:38:41 +0000

    Initial configuration of NextJs, TypeScript and SASS.

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ig News</title>
+      </Head>
+
+      <h1>
+        IgNews
+      </h1>
+    </>
+  )
+}
