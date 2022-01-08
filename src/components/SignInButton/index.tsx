ea77691cdf308f9b0daf74f4b27f2fa86782d2c0
David Barros <dwbarros@gmail.com>
2022-01-08 23:06:44 +0000

    Save user on FaunaDB.

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,13 +1,14 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import { signIn, signOut, useSession } from 'next-auth/react';
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
     const { data: session } = useSession();
 
     return session ? (
-        <button 
+        <button
             type="button"
             className={styles.signInButton}
             onClick={() => signOut()}    
