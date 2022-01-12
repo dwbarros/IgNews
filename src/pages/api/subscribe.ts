66e80035b2669dde6f2315836482ec2afda22059
David Barros <dwbarros@gmail.com>
2022-01-12 16:12:07 +0000

    Create webhook event listener.

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -50,7 +50,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         }
 
 
-
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
             customer: customerId,
             payment_method_types: ['card'],
