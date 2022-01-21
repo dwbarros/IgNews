e8c7fa6b687cadffc4ccc4429de5e57841e75877
David Barros <dwbarros@gmail.com>
2022-01-21 11:35:37 +0000

    List posts on page Posts from Prismic API.

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,24 @@
-import Head from 'next/head'
-import styles from './styles.module.scss'
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { getPrismicClient } from '../../services/prismic';
+import styles from './styles.module.scss';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 
-export default function Posts() {
+
+type Post = {
+    slug: string,
+    title: string,
+    excerpt: string,
+    updatedAt: string,
+}
+
+interface PostsProps {
+    posts: Post[]
+}
+
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -10,23 +27,47 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="#">
-                        <time>07 de fevereiro de 2022</time>
-                        <strong>Um título bem interessante para o post</strong>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium nobis, fugit quo corrupti ut tempora incidunt alias nam? Odio natus placeat odit, delectus architecto ut? Modi culpa praesentium neque. Iure.</p>
-                    </a>
-                    <a href="#">
-                        <time>07 de fevereiro de 2022</time>
-                        <strong>Um título bem interessante para o post</strong>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium nobis, fugit quo corrupti ut tempora incidunt alias nam? Odio natus placeat odit, delectus architecto ut? Modi culpa praesentium neque. Iure.</p>
-                    </a>
-                    <a href="#">
-                        <time>07 de fevereiro de 2022</time>
-                        <strong>Um título bem interessante para o post</strong>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium nobis, fugit quo corrupti ut tempora incidunt alias nam? Odio natus placeat odit, delectus architecto ut? Modi culpa praesentium neque. Iure.</p>
-                    </a>
+                    { posts.map(post => (
+                        <a key={post.slug} href="#">
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    ))}
                 </div>
             </main>
         </>
     );
-} 
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient();
+
+    const response = await prismic.query<any>([
+        Prismic.predicates.at('document.type', 'post')
+    ], {
+        fetch: ['post.title', 'post.content'],
+        pageSize: 100,
+    })
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        };
+    });
+
+    return {
+        props: {
+            posts
+        }
+    }
+}
+
+
