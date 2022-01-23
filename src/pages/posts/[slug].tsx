08dd4ce8207443bfcfea2612ab450911aa114968
David Barros <dwbarros@gmail.com>
2022-01-23 22:59:34 +0000

    Check Subscription Status.

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -39,6 +39,16 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     const session = await getSession({ req });
     const { slug } = params;
 
+    if(!session.activeSubscription) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            }
+        }
+    }
+
+
     const prismic = getPrismicClient(req);
 
     const response = await prismic.getByUID<any>('post', String(slug), {});
